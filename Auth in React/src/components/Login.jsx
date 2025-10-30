700bc08d5199b09ed357d4a90973e20131ba5492
Ahmad Siddique <asshikrani66@gmail.com>
2025-10-30 17:08:36 +0500

    Designing of login and signup.

---
+++ Auth in React/src/components/Login.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+const Login = () => {
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        {...register("name", { required: true, maxLength: 30 })}
+      />
+      {errors.name && errors.name.type === "required" && (
+        <span>This is required</span>
+      )}
+      {errors.name && errors.name.type === "maxLength" && (
+        <span>Max length exceeded</span>
+      )}
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" {...register("email", {required : true})}/>
+      {errors.email && errors.email.type === "required" && (
+        <span>Input Email.</span>
+      )}
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" {...register("password", {required : true, minLength : 8})} />
+      {errors.password && errors.password.type === "required" && (
+        <span>Input Password</span>
+      )}
+      {errors.password && errors.password.type === "minLength" && (
+        <span>Password is too short.</span>
+      )}
+      <input type="submit" />
+    </form>
+    </div>
+  )
+}
+
+export default Login
